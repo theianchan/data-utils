487d43ee5cbb426bf21411bd92219aec4304b21f
Ian Chan <hi@theianchan.com>
2018-02-13 18:33:09 -0800

    Eval functions for linear models

---
+++ models.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+def root_mean_squared_error(y_pred, y):
+    """Calculates root mean squared error"""
+
+    return np.sqrt(np.mean((y_pred - y) ** 2))
+
+
+def lm_evaluate(lm, x=None, y=None):
+    """Takes a linear model and prints the intercept,
+    coefficients, R-squared value, and RMSE
+
+    Returns R-squared value and RMSE in a tuple"""
+
+    print("Intercept: {}\nCoefficients: {}".format(
+        lm.intercept_, lm.coef_
+    ))
+    if (x is not None) and (y is not None):
+        r_squared = lm.score(x, y)
+        rmse = root_mean_squared_error(lm.predict(x), y)
+        print("R-Squared Value: {}\nRMSE: {}".format(
+            r_squared, rmse
+        ))
+        return (r_squared, rmse)
---
+++ stats.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as stats
+
+def pairwise_t_test():
+    pass
+
+def df_anova():
+    pass
